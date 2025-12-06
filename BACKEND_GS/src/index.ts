cb9bf0e93388bb100b8afa5ed33ea94e77825a28
Gustavo <gustavoblancosandoval059@gmail.com>
2025-12-06 11:50:43 -0400

    feat: cambios para deploy

--- BACKEND_GS/src/index.ts
+++ BACKEND_GS/src/index.ts
@@ -3,8 +3,15 @@ dotenv.config();
 
 import app from "./app";
 
-const PUERTO = process.env.PUERTO || 3000;
+const PUERTO = process.env.PORT || 3000;
 
-app.listen(PUERTO, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PUERTO}/api/v1/autenticacion/login`);
-});
+// Vercel maneja el servidor por su cuenta.
+// Solo arrancamos el servidor manualmente si NO estamos en producción (modo local)
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PUERTO, () => {
+      console.log(`🚀 Servidor corriendo en http://localhost:${PUERTO}/api/v1/autenticacion/login`);
+    });
+}
+
+// ⚠️ ESTO ES OBLIGATORIO PARA VERCEL
+export default app;
